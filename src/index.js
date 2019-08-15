634ff9779c4e0b4e7da9650c0a64a5d73f4e76bb
Russel <russel.tsang@gmail.com>
2019-08-15 16:34:16 -0400

    refactor code

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     field.createFieldWithHoldBoxAndNextBoxes(fieldContainer);
 
     let game = new Game();
+    game.keyListener();
     game.play(fieldContainer);
 
 });
